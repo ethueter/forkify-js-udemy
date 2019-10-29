8d9850ae821be1197135a952604dcb20810fd713
Tyler Hueter <tylerhueter08@gmail.com>
2019-10-29 19:30:17 -0400

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+import Search from './models/Search';
+import * as searchView from './views/searchView'
+import { elements, renderLoader, clearLoader } from './views/base';
+const state = {}
+
+const controlSearch = async () => {
+    // 1. Get query from view
+    const query = searchView.getInput();
+    console.log(query)
+
+    if (query) {
+        // 2. New search object and add to state
+        state.search = new Search(query);
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+        // 4. Serch for recipes
+        await state.search.getResults();
+
+        // 5. Redner results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    controlSearch()
+});
+
+elements.searchResPages.addEventListener('click', (e) => {
+    const btn = e.target.closest('.btn-inline')
+    if (btn) {
+        const goToPage= parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage)
+    }
+})
+
+
+
+
+
+
---
+++ src/js/interview.js
@@ -0,0 +1,208 @@
+
+const frequencies = (arr) => {
+    return arr.reduce((obj, el) => {
+        obj[el] ? ++obj[el] : obj[el] = 1;
+        return obj
+    }, {})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const multipleCheck = (num, factor) => {
+//     return num % factor === 0 ? true : false
+// }
+
+// const fizzBuzz = (num) => {
+//     let finalAnswer = []
+
+//     for(let i = 1; i < num; i++) {
+//         switch (true) {
+//             case multipleCheck(i, 15):
+//                 finalAnswer.push('fizzbuzz');
+//                 break;
+//             case multipleCheck(i, 5):
+//                 finalAnswer.push('buzz');
+//                 break;
+//             case multipleCheck(i, 3):
+//                 finalAnswer.push('fizz');
+//                 break;
+//             default:
+//                 finalAnswer.push(i)
+//         }
+//     }
+//     return finalAnswer.join(', ')
+// }
+
+
+
+
+// let answer = []
+// for (let i = 1; i < num + 1; i++) {
+//     
+// return answer.join(',')
+
+
+
+// const newObj = (str) => {
+//     let obj = {}
+//     for (let i in str) {
+//         console.log(i)
+//         let letter = str.charAt(i).toLowerCase()
+//         if (obj[letter]) {
+//             obj[letter] += 1
+//         } else {
+//             obj[letter] = 1
+//         }
+//     }
+//     return obj
+// }
+
+
+
+// const anagram = (str1, str2) => {
+//     let a = newObj(str1)
+//     let b = newObj(str2)
+//     if (Object.keys(a).length != Object.keys(b).length) {
+//         return false
+//     }
+//     for (let item in a) {
+//         if (a[item] !== b[item]) {
+//             return false
+//         } 
+//     }
+//     cleanUpInput(str1)
+//     return true
+    
+// }
+
+
+
+// const cleanUpInput = (inputStr) => {
+//     return inputStr.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('')
+    
+// }
+
+
+// const anagram = (str1, str2) => {
+//     let first = cleanUpInput(str1)
+//     let second = cleanUpInput(str2)
+//     return first === second ? true : false
+// }
+
+
+
+
+
+
+
+export default frequencies;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//finding max number of consecutive 0 in binary
+
+//function test(a) {
+// let arr = string.toString(2).split('');
+// let count = 0;
+// let arr1 = []
+// console.log(arr)
+
+// if (!arr.includes('0')) {
+//     return 0
+// } else {
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[arr.length - 1] == 0) {
+//             arr.pop()
+//         }
+//     }
+// }
+
+// for (let j = 0; j < arr.length; j++) {
+//     switch (arr[j]) {
+//         case '0':
+//             console.log('test1')
+//             count = count + 1
+//             break;
+//         case '1':
+//             console.log('test2')
+//             arr1.push(count)
+//             count = 0
+//             break;
+//         default:
+//             console.log('nothing works')
+//     }
+// }
+// return Math.max(...arr1)
+// }
+
+// const howManyMovies = (movies, flightLength) => {
+//     console.log(movies)
+//     let lengthMin = flightLength * 60
+//     let twoMovies = movies.map(movie => movie.runtime)
+//     for (let i = 0; i < twoMovies.length; i++) {
+
+//     }
+
+
+//     console.log(twoMovies)
+
+// }
